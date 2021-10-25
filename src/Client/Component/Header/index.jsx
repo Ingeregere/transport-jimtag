b6d3ea1058e248692fee0dbc8d2585f155d738bd
unknown <ingeregere@gmail.com>
2021-10-25 22:46:59 +0200

    main project

--- src/Client/Component/Header/index.jsx
+++ src/Client/Component/Header/index.jsx
@@ -12,7 +12,7 @@ const NavbarHeader = () => {
 
             <Navbar.Brand as={Link} to={'/'} >
                 <img
-                    src="/logo.jpeg"
+                    src="/logo.png"
                     width="100"
                     height="40"
                     className="d-inline-block align-top logo"
