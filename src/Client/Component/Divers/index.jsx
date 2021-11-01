44eace3c7c3e34e909bfdb1c10021059a626924c
unknown <ingeregere@gmail.com>
2021-11-01 05:51:34 +0200

    updated

--- src/Client/Component/Divers/index.jsx
+++ src/Client/Component/Divers/index.jsx
@@ -25,26 +25,25 @@ const Product = () => {
                                {values === []? "no product" : values.map((product) =>(
                                    <Col lg={6} md={6} sm={6} xs={12} key={product.id}>
 
-
                                    <Card className={'mb-2 mt-2 mr-2 ml-2 cardProduct'}>
-                                   <Row>
-                                   <Col xs={12} md={6} className={'w-100'}>
-                                   <ShowImageTransport item={'transport'}  method={'viewImageTransportById'} getId={product} />
-                                   </Col>
-                                   <Col xs={12} md={6} >
-                                   <Card.Body className={'cardbodymain'}>
-                                   <p></p>
-                                   <h6>Marque : {product.brand} </h6>
-                                   <h6>Model : {product.model}</h6>
-                                   <h6>Fonction: {product.box}</h6>
-                                   <h6>Année : {product.dateRegistration}</h6>
-                                   <h6>Km : {product.kilometer}</h6>
-                                   <Link to={`/detail/${product.id}`}>
-                                   <Button variant="primary" >En savoir plus</Button>
-                                   </Link>
-                                   </Card.Body>
-                                   </Col>
-                                   </Row>
+                                       <Row>
+                                           <Col xs={12} md={6} className={'w-100'}>
+                                           <ShowImageTransport item={'transport'}  method={'viewFirstImageTransportById'} getId={product} />
+                                           </Col>
+                                           <Col xs={12} md={6} >
+                                           <Card.Body className={'cardbodymain'}>
+                                           <p></p>
+                                           <h6>Marque : {product.brand} </h6>
+                                           <h6>Model : {product.model}</h6>
+                                           <h6>Fonction: {product.box}</h6>
+                                           <h6>Année : {product.dateRegistration}</h6>
+                                           <h6>Km : {product.kilometer}</h6>
+                                           <Link to={`/detail/${product.id}`}>
+                                           <Button variant="primary" >En savoir plus</Button>
+                                           </Link>
+                                           </Card.Body>
+                                           </Col>
+                                       </Row>
                                    </Card>
 
 
